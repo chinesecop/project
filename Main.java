84cdf243277c9f7978d9b8d0f67010541cba3811
chinesecop <68824326+chinesecop@users.noreply.github.com>
2020-08-07 11:32:44 +0400

    Update Main.java
    - changed task 6*, by adding a setter
    - changed class name from employee to employees

--- Main.java
+++ Main.java
@@ -1,53 +1,51 @@
 public class Main {
-
-    private static void mean(Employee [] elements) {
+// Task 7*
+    private static void mean(Employees[] elements) {
         float aSalary = 0;
         float aAge = 0;
         int length = elements.length;
 
         for (int i = 0; i < length; i++) {
-            Employee eL = elements[i];
-            aSalary = aSalary + eL.getSalary();
-            aAge = aAge + eL.getAge();
+            Employees e0 = elements[i];
+            aSalary = aSalary + e0.getSalary();
+            aAge = aAge + e0.getAge();
         }
         System.out.println("\nAverage salary: " + aSalary / length + "\n" +
                 "Average age: " + aAge / length + "\n");
     }
-
-    private static void salaryIncrease (Employee[] elements) {
+// Task 6*
+    private static void salaryIncrease (Employees[] elements) {
         for (int i = 0; i < elements.length; i++) {
-            Employee eL2 = elements[i];
-            int money = 5000;
-            int increase = 0;
-            if (eL2.getAge() > 45) {
-                increase = eL2.getSalary() + money;
-                    System.out.println(increase);
+            Employees e1 = elements[i];
+            int increase = 5000;
+            if (e1.getAge() > 45){
+                e1.setSalary(e1.getSalary() + increase);
             }
         }
     }
-
-   private static void moreThanForty(Employee[] elements){
+// Task 5    
+   private static void moreThanForty(Employees[] elements){
 
         for (int i = 0; i < elements.length; i++) {
-            Employee x = elements[i];
-            if (x.getAge() > 40) {
-                System.out.println(x.getFullNameAgeInfo());
+            Employees e2 = elements[i];
+            if (e2.getAge() > 40) {
+                System.out.println(e2.getFullNameAgeInfo());
             }
         }
     }
-
-    public static void main(String[] args) {
-        Employee c0 = new Employee("Andrey V.A.", 50000, 25);
-        Employee c1 = new Employee("Maria K.S.", 85000, 39);
-        Employee c2 = new Employee("Valeria S.S.", 64000, 41);
-        Employee c3 = new Employee("Eugene K.M.", 120000, 46);
-        Employee c4 = new Employee("Slava P.S.", 250000, 52);
-
+    
+   public static void main(String[] args) {
+        Employees c0 = new Employees("Andrey V.A.", 50000, 25);
+        Employees c1 = new Employees("Maria K.S.", 85000, 39);
+        Employees c2 = new Employees("Valeria S.S.", 64000, 41);
+        Employees c3 = new Employees("Eugene K.M.", 120000, 46);
+        Employees c4 = new Employees("Slava P.S.", 250000, 52);
+// Task 4
         System.out.println("Employee: " + c0.getFullNameAgeInfo() + "\n");
 
-        Employee elements[] = new Employee[]{c0, c1, c2, c3, c4};
+        Employees elements[] = new Employees[]{c0, c1, c2, c3, c4};
+       
         moreThanForty(elements);
-        mean(elements);
-        salaryIncrease(elements);
+        mean(elements)
     }
 }
